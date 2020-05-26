4d05a421ff4511c1ba8061f349ba668cb2e51bad
Sayan-Roy-729 <65708786+Sayan-Roy-729@users.noreply.github.com>
2020-05-26 21:18:22 +0530

    Initial Commit

---
+++ confirm_order.php
@@ -0,0 +1,16 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "zomato");
+
+$order_id = $_GET['order_id'];
+$amount=$_GET['amount'];
+
+$query = "UPDATE orders SET amount = '$amount', status =1 WHERE order_id LIKE '$order_id'";
+
+try{
+    mysqli_query($conn, $query);
+    header('Location: success.php');
+}catch(Exeption $e){
+    header('Location: order_details.php?order+id='.$order_id);
+}
+
+?>
